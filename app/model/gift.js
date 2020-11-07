9a57fd4297cacc9cb671c58c8aeaa3db2b3240e4
T007 <1255965856@qq.com>
2020-11-07 21:59:34 +0800

    删除和编辑管理员

--- app/model/gift.js
+++ app/model/gift.js
@@ -1,40 +1,40 @@
-'use strict';
+module.exports = app => {
+    const { STRING, INTEGER, DATE, ENUM, TEXT } = app.Sequelize;
 
-module.exports =(app) => {
-    const { INTEGER, STRING, DATE, ENUM, TEXT } = app.Sequelize;
     const Gift = app.model.define('gift', {
         id: {
             type: INTEGER(20),
             primaryKey: true,
             autoIncrement: true
-          },
-          name: {
-            type: STRING(100),
+        },
+        name: {
+            type: STRING,
             allowNull: false,
             defaultValue: '',
             comment: '礼物名称'
-          },
-          image: {
+        },
+        image: {
             type: STRING,
             allowNull: true,
             defaultValue: '',
-            comment: '礼物图标'
-          },
-          coin:{
+            comment: '礼物图标',
+            get(){
+                const ctx = app.createAnonymousContext();
+                let url = this.getDataValue('image')
+                url = app.config.webUrl + url
+                return url
+            }
+        },
+        coin: {
             type: INTEGER,
             allowNull: false,
             defaultValue: 0,
-            comment: '金币',
-          },
-          created_time: {
-              type:DATE,
-              get(){
-                  return app.formatTime(this.getDataValue('created_time'))
-              }
-          },
-          updated_time: DATE,
-    })
+            comment: '金币'
+        },
+        created_time: DATE,
+        updated_time: DATE,
 
+    });
 
-    return Gift
-  }
+    return Gift;
+};
